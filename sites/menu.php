5bbd75cdfab09cd2acfec66f63723db885b4afa8
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-14 15:26:25 +0200

    Added Bootstrap

---
+++ sites/menu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System zarządzania zleceniami</title>
+    <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col text-uppercase">
+                2021-07-14
+            </div>
+            <div class="col text-uppercase">
+                Nowe
+            </div>
+            <div class="col">
+                Bartłomiej
+            </div>
+            <div class="col">
+                Żółtek
+            </div>
+        </div>
+    </div>
+
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
