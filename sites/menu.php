020027ef546c0c923e516a1a29fa4c77fd14a185
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-14 20:20:08 +0200

    Added file menu.php and table layout that will display all orders

--- sites/menu.php
+++ sites/menu.php
@@ -6,25 +6,95 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>System zarządzania zleceniami</title>
     <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="../css/site.css">
+    <link rel="stylesheet" href="../css/show_all.css">
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col text-uppercase">
-                2021-07-14
-            </div>
-            <div class="col text-uppercase">
-                Nowe
-            </div>
-            <div class="col">
-                Bartłomiej
-            </div>
-            <div class="col">
-                Żółtek
+
+        <div class="dashboard">
+            <div class="row text-center">
+                <div class="col dashboard__button bg-green ">
+                    Nowe zlecenie
+                </div>
+                <div class="col dashboard__button bg-silver">
+                    Status
+                </div>
+                <div class="col dashboard__button bg-gray">
+                    Sortus według
+                </div>
+                <div class="col dashboard__button bg-black">
+                    Wyświetl Klientów
+                </div>
             </div>
         </div>
+
+        <div class="label-header">
+            <div class="row text-center">
+                <div class="col-1 label-header__cell bg-green">
+                    Nr zlecenia
+                </div>
+                <div class="col-1 label-header__cell bg-green date">
+                    Data
+                </div>
+                <div class="col-1 label-header__cell bg-green status">
+                    Status
+                </div>
+                <div class="col label-header__cell bg-green">
+                    Imię
+                </div>
+                <div class="col-2 label-header__cell bg-green">
+                    Nazwisko
+                </div>
+                <div class="col-1 label-header__cell bg-green date">
+                    Numer telefonu
+                </div>
+                <div class="col-2 label-header__cell bg-green">
+                    Sprzęt
+                </div>
+                <div class="col-1 label-header__cell bg-green">
+                    Więcej opcji
+                </div>
+            </div>
+        </div>
+        <div class="data">
+            <?php
+                for ($i = 0; $i < 20; $i++) :
+            ?>
+            
+            <div class="row text-center">
+                <div class="col-1 allign-center data__cell">
+                    1
+                </div>
+                <div class="col-1 date data__cell">
+                    2021-07-14
+                </div>
+                <div class="col-1 status data__cell">
+                    Zakończone
+                </div>
+                <div class="col data__cell">
+                    Bartłomiej
+                </div>
+                <div class="col-2 data__cell">
+                    Żółtek
+                </div>
+                <div class="col-1 date data__cell">
+                    666222444
+                </div>
+                <div class="col-2 data__cell">
+                    Telefon
+                </div>
+                <div class="col-1 bg-green data__button">
+                    Więcej
+                </div>
+            </div>
+
+            <?php
+                endfor;
+            ?>
+        </div>
     </div>
+    
 
     <script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
     
