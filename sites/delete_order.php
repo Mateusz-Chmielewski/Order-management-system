6bb1c0b179084661ee593c70a8aa684f8a2041d2
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-20 14:48:22 +0200

    Added the ability to delete order

---
+++ sites/delete_order.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    try {
+        require_once '../connection/connection.php';
+        $connection = openConnection();
+        $ID = $_GET['ID'];
+        $tsql = "DELETE FROM zlecenia WHERE ID_zlecenia='$ID'";
+        $deleteOrder = sqlsrv_query($connection, $tsql);
+
+        if (!$deleteOrder)
+            throw new Exception;
+                
+        sqlsrv_free_stmt($deleteOrder);
+        
+        sqlsrv_close($connection);
+    } catch (Exception $e) {
+        $_SESSION['confirmation'] = '<span class="error">Błąd usuwania zlecenia</span>';
+        header('Location: menu.php');
+        exit();
+    }
+
+    $_SESSION['confirmation'] = "Usunięto zlecenie";
+    header('Location: menu.php');
+?>
