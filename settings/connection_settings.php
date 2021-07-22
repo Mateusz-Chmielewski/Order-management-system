2fae6eb86bf7af7286581af4f5a40ddffbb49ddd
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-22 15:39:58 +0200

    Added the new database form

--- settings/connection_settings.php
+++ settings/connection_settings.php
@@ -70,6 +70,59 @@
 
     </div>
 
+    <div class="container">
+        <div class="form__header">
+            Nowa <span class="color-green">Baza danych</span> 
+        </div>
+
+        <form action="new_database.php" method="post">
+
+            <div class="row form-group">
+                <div class="col-3 form__cell__header form__cell bg-green">
+                    <label for="host" class="form__input">Host</label>
+                </div>
+                <div class="col form__cell">
+                    <input type="text" class="form-control" id="host" name="host" placeholder="Wpisz nazwę serwera" value="<?php echo $db_host; ?>">
+                </div>
+            </div>
+
+            <div class="row form-group">
+                <div class="col-3 form__cell__header form__cell bg-silver">
+                    <label for="name" class="form__input">Nazwa</label>
+                </div>
+                <div class="col form__cell">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Zostanie utworzona nowa baza danych o tej nazwie" value="">
+                </div>
+            </div>
+
+            <div class="row form-group">
+                <div class="col-3 form__cell__header form__cell bg-gray">
+                    <label for="user" class="form__input"></label>
+                </div>
+                <div class="col form__cell">
+                    <input type="text" class="form-control" id="user" name="user" placeholder="Wpisz nazwę użytkownika" value="<?php echo $db_user; ?>">
+                </div>
+            </div>
+
+            <div class="row form-group">
+                <div class="col-3 form__cell__header form__cell bg-black">
+                    <label for="password" class="form__input">Hasło</label>
+                </div>
+                <div class="col form__cell">
+                    <input type="text" class="form-control" id="password" name="password" placeholder="Wpisz hasło użytkownika" value="<?php echo $db_password; ?>">
+                </div>
+            </div>
+
+            <div class="row form-group">
+                <div class="col invisible"></div>
+                <input type="submit" value="Nowa" class="col-2 bg-green data__button btn m-2">
+                <input type="button" value="Anuluj" class="col-2 bg-silver data__button btn m-2" onclick="window.location.href='../index.php'">
+            </div>
+
+        </form>
+
+    </div>
+
     <script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
     
 </body>
